5fb86240145ba0bacd708e8b679d4bdb11c59440
YOGESHGUPTA033 <gupta.yogesh033@gmail.com>
2024-02-01 23:24:28 +0530

    ObjectError

--- Crecket Game/game.js
+++ Crecket Game/game.js
@@ -1,4 +1,11 @@
 
+
+let score = {
+  win: 0,
+  lost: 0,
+  tie: 0,
+};
+
 function generateComputerChoice(){
   let randomNumber=Math.random() *3;
   let computerChoice;      
@@ -19,34 +26,43 @@ function getResult(userMove, ComputerMove){
     if (userMove='bat'){
       
       if (ComputerMove === 'bat'){
+        score.tie++;
         return `It's a Tie`;
       }
       else if (ComputerMove === 'ball'){
+        score.win++;
         return`User Won`;
       }
       else if (ComputerMove === 'stump'){
+        score.lost++;
         return`Computer Won`;
       }
     }
     else if (userMove='ball'){
 
       if (ComputerMove === 'bat'){
+        score.lost++;
         return  `Computer Won`;
       }
       else if (ComputerMove === 'ball'){
+        score.tie++;
         return `Its a tie`;
       }
       else if (ComputerMove === 'stump'){
+        score.win++;
         return `User Won`;
       }
       else{
         if (ComputerMove === 'bat'){
+          score.lost++;
           return `Computer won`;
         }
         else if (ComputerMove === 'ball'){
+          score.win++;
           return`User Won`;
         }
         else if (ComputerMove === 'stump'){
+          score.lost++;
           return`Its a tie`;
         }
       }
@@ -57,3 +73,4 @@ function getResult(userMove, ComputerMove){
 function showResult(userMove, ComputerMove, result){
   alert(`Your Choice is ${userMove} Computer Choice is ${ComputerMove} According to this the Result is  ${result}`);
 }
+console.log(score);
